a733af0f9ec92e14aa3c17f3a15338f34afb34d0
WillySand <60334511+WillySand@users.noreply.github.com>
2022-06-12 14:33:46 +0700

    Update models.py

--- reader/readerApp/models.py
+++ reader/readerApp/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Mahasiswa(models.Model):
    npm = models.CharField(max_length = 10,primary_key = True)
    nama = models.CharField(max_length = 50)
-    class Meta:
-        managed = False
-        db_table = 'updateApp_mahasiswa'
+   class Meta:
+      managed = False
+      db_table = 'updateApp_mahasiswa'
